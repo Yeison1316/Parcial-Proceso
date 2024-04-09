173e795d6ba8658df4f193a3938cca2cd2a6a117
Yeison1316 <143632052+Yeison1316@users.noreply.github.com>
2024-04-09 12:16:20 -0500

    Se crearon objeos vacios de prueba

--- src/main/java/com/ufpso/tienda/article/repository/ArticleRepository.java
+++ src/main/java/com/ufpso/tienda/article/repository/ArticleRepository.java
@@ -3,6 +3,11 @@ package com.ufpso.tienda.article.repository;
 import com.ufpso.tienda.article.model.Article;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ArticleRepository extends CrudRepository<Article, Long> {
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+    Optional<User> findByEmail(String email);
+    List<User> findByBirthDay(LocalDate BirthDay);
 }
