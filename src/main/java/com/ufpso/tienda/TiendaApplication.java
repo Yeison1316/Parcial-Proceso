628c259a36bd84413b23637ddf7e943b7b6503a7
Yeison1316 <143632052+Yeison1316@users.noreply.github.com>
2024-04-09 13:38:55 -0500

    listo

--- src/main/java/com/ufpso/tienda/TiendaApplication.java
+++ src/main/java/com/ufpso/tienda/TiendaApplication.java
@@ -1,10 +1,6 @@
 package com.ufpso.tienda;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TiendaApplication {
