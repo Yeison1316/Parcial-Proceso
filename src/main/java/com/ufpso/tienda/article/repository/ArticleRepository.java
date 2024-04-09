97f70ce0cd25147268012ac475e96be9de002461
Yeison1316 <143632052+Yeison1316@users.noreply.github.com>
2024-04-09 12:21:18 -0500

    arreglos al objeto optional

--- src/main/java/com/ufpso/tienda/article/repository/ArticleRepository.java
+++ src/main/java/com/ufpso/tienda/article/repository/ArticleRepository.java
@@ -3,11 +3,10 @@ package com.ufpso.tienda.article.repository;
 import com.ufpso.tienda.article.model.Article;
 import org.springframework.data.repository.CrudRepository;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface ArticleRepository extends CrudRepository<Article, Long> {
-    Optional<User> findByEmail(String email);
-    List<User> findByBirthDay(LocalDate BirthDay);
+    Optional<Article> findByName(String name);
+    List<Article> findByDescription(String description);
 }
