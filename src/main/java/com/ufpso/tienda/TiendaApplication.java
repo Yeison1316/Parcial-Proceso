a91389760dd954b08df19ac0e099ec98192ac512
Yeison1316 <143632052+Yeison1316@users.noreply.github.com>
2024-04-09 12:06:02 -0500

    cambios en el archivo TiendaAplicacion

--- src/main/java/com/ufpso/tienda/TiendaApplication.java
+++ src/main/java/com/ufpso/tienda/TiendaApplication.java
@@ -12,14 +12,4 @@ public class TiendaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TiendaApplication.class, args);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer(){
-		return new WebMvcConfigurer(){
-			@Override
-			public void addCorsMappings(CorsRegistry registry){
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
-			}
-		};
-	}
 }
